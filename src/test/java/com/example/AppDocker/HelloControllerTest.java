d8ea51d45d25690b13b9ed0c0a241e84a8955d13
ABC <kirilko150@gmail.com>
2025-04-09 10:29:16 +0400

    first commit

---
+++ src/test/java/com/example/AppDocker/HelloControllerTest.java
@@ -0,0 +1,16 @@
+package com.example.AppDocker;
+
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class HelloControllerTest {
+
+    @Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+}
