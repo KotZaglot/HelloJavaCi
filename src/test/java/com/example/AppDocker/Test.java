9aefc2fa15597db85b56da9b54b05042b290ff74
ABC <kirilko150@gmail.com>
2025-04-09 10:53:22 +0400

    test

---
+++ src/test/java/com/example/AppDocker/Test.java
@@ -0,0 +1,10 @@
+package com.example.AppDocker;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Test {
+    @org.junit.jupiter.api.Test
+    public void dummyTest() {
+        assertTrue(true);
+    }
+}
